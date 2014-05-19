7ef2601a1031775e330dcbee1a28d1a8655a57f5
Onur Unal <onur.unal@smartmobilefactory.com>
2014-05-19 15:08:39 +0200

    Revert "sadf"
    This reverts commit 1e28c63821b80058b813fa92a1eca76abb79c3a7.

--- Anypic-iOS/Anypic/PAPFindFriendsViewController.m
+++ Anypic-iOS/Anypic/PAPFindFriendsViewController.m
@@ -27,11 +27,10 @@ typedef enum {
 @property (nonatomic, strong) NSMutableDictionary *outstandingCountQueries;
 @end
 
-/*
 static NSUInteger const kPAPCellFollowTag = 2;
 static NSUInteger const kPAPCellNameLabelTag = 3;
 static NSUInteger const kPAPCellAvatarTag = 4;
-static NSUInteger const kPAPCellPhotoNumLabelTag = 5;*/
+static NSUInteger const kPAPCellPhotoNumLabelTag = 5;
 
 @implementation PAPFindFriendsViewController
 @synthesize headerView;
